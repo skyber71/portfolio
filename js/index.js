cac9ab12b0d752b77caebaefc9751a73acb06b41
Kiran Yadav <mail.skyber71@gmail.com>
2022-07-14 11:37:30 +0530

    Done

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("Kiran YAdav");
+var typed = new Typed(".typing", {
+    strings: ["CSE Student.","Web Developer.","Coder."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
